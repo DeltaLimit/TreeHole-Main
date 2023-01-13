cc20a5063b72ca9e980ff848b5b6fbdc6d447105
DeltaLimit <deltalimit@outlook.com>
2023-01-13 18:32:08 +0800

    Use Gin to handle http

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
 func init() {
 
 }
 
 func main() {
+	r := gin.Default()
 
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"urIP": c.ClientIP(),
+		})
+	})
+	err := r.Run()
+	HandleError(err)
+}
+
+func HandleError(e error) {
+	log.Fatalln(e)
+	return
 }
