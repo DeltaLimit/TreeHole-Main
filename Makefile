a556bbc847aeb1b4b98186aafbaaf0917b70dc6a
DeltaLimit <deltalimit@outlook.com>
2023-01-15 00:32:54 +0800

    Optimize makefile and router

--- Makefile
+++ Makefile
@@ -1,11 +1,12 @@
 WINDOWS = treehole-windows.exe
 LINUX = treehole-linux
 
-all:
+linux:
 	@echo "Building linux binary file..."
 	go build -o $(LINUX) main.go
 	@echo "Linux binary file build successful!"
 
+windows:
 	@echo "Building windows binary file..."
 	CGO_ENABLED=1 GOOS=windows GOARCH=amd64  go build -o $(WINDOWS) main.go
 	@echo "Windows binary file build successful!"
--- router/router.go
+++ router/router.go
@@ -6,13 +6,18 @@ import (
 	"time"
 )
 
+const (
+	title      = "树洞系统"
+	githubAddr = "https://github.com/DeltaLimit/Treehole-Main/"
+)
+
 func InitRouter(router *gin.Engine) {
 	router.LoadHTMLGlob("static/html/*")
 
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{
-			"title":       "树洞系统",
-			"github_addr": "https://github.com/DeltaLimit/TreeHole-Main/",
+			"title":       title,
+			"github_addr": githubAddr,
 			"time":        time.Now(),
 		})
 	})
